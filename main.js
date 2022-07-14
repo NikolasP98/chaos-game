d6ca3082938c8d245cc75d8a31c563ccca7ddc5a
Nikolas Pinon <nikolas.pinon98@gmail.com>
2022-07-14 17:39:09 -0600

    added gui

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 import Chaos from './classes/chaos';
+import GUI from 'lil-gui';
 
+const gui = new GUI();
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let chaos;
 
+const settings = {
+	points: 3,
+};
+
+gui.add(settings, 'points', 3, 10, 1).onFinishChange(() => {
+	setup();
+});
+
 const setup = () => {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
-	chaos = new Chaos(3, ctx);
+	chaos = new Chaos(settings.points, ctx);
 
 	// start animation
 	requestAnimationFrame(animate);
